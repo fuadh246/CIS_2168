1eed0c77bd5fb634c32aa099eff8c63e39d481ae
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-01-14 10:18:33 -0500

    added a folder

---
+++ Hello/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ArrayList <String> names = new ArrayList<>();
+        names.add("Fuad");
+        names.add("Hassan");
+        for (String name:names)
+             {
+                 System.out.println(name);
+             }
+    }
+}
