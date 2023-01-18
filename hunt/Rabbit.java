ad966e489c6ac99474dd176ad43457d0b79be6c2
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-01-18 11:39:01 -0500

    removing fox stuct.

--- hunt/Rabbit.java
+++ hunt/Rabbit.java
@@ -12,7 +12,7 @@ public class Rabbit extends Animal {
     int decideMove() {
         currentDirection = Model.random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
         canSeeFoxNow = false;
-        for (int i = Model.MIN_DIRECTION; i < Model.MAX_DIRECTION; i++) {
+        for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
             if (look(i) == Model.FOX) {
                 canSeeFoxNow = haveSeenFox = true;
                 directionToFox = i;
@@ -22,10 +22,10 @@ public class Rabbit extends Animal {
 //        System.out.println("sid "+distanceToFox);
         if (haveSeenFox) {
 //            System.out.println(haveSeenFox);
-            if (canMove(oppDic(directionToFox))) {
+            if (canMove(Model.turn(directionToFox,4))) {
 //                System.out.println(canMove(oppDic(directionToFox)));
 //                System.out.println("can move");
-                return oppDic(directionToFox);
+                return Model.turn(directionToFox,4);
             } else { // rabbit was here--where did it go?
 //                System.out.println("cann't move");
 
@@ -51,34 +51,5 @@ public class Rabbit extends Animal {
             }
         }
         return Model.STAY;
-
-    }
-
-    int oppDic(int direction) {
-        if (direction == Model.N) {
-            int next_step = Model.random(Model.MIN_DIRECTION,
-                    Model.MAX_DIRECTION);
-            if (next_step != Model.N) {
-                return next_step;
-            }
-        } else if (direction == Model.NE) {
-            return Model.SW;
-        } else if (direction == Model.E) {
-            return Model.W;
-        } else if (direction == Model.SE) {
-            return Model.NW;
-        } else if (direction == Model.S) {
-            return Model.N;
-        } else if (direction == Model.SW) {
-            return Model.NE;
-        } else if (direction == Model.W) {
-            return Model.E;
-        } else if (direction == Model.NW) {
-            return Model.SW;
-        }
-//        else if(direction==Model.BUSH){return Model.N;}
-        else {
-            return Model.STAY;
-        }
     }
 }
