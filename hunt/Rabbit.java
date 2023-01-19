3c12b1fc61c07629528ea991bf5981e4c151b256
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-01-18 22:25:20 -0500

    Fox and Rabbit done 01/18/23

--- hunt/Rabbit.java
+++ hunt/Rabbit.java
@@ -13,32 +13,27 @@ public class Rabbit extends Animal {
         currentDirection = Model.random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
         canSeeFoxNow = false;
         for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
-                if (look(i) == Model.FOX) {
-                    directionToFox = i;
+            if (look(i) == Model.FOX) {
+                directionToFox = i;
 
-                    if (canMove(Model.turn(directionToFox, 3)) && distance(i) != 3)
-                        return Model.turn(directionToFox, 3);
-                    else if (canMove(Model.turn(directionToFox, 5)) && distance(i) != 5)
-                        return Model.turn(directionToFox, 5);
-                    else if (canMove(Model.turn(directionToFox, 2)) && distance(i) != 2)
-                        return Model.turn(directionToFox, 2);
-                    else if (canMove(Model.turn(directionToFox, 6)) && distance(i) != 6)
-                        return Model.turn(directionToFox, 6);
-                    else if (canMove(Model.turn(directionToFox, 7)) && distance(i) != 7)
-                        return Model.turn(directionToFox, 7);
-                    else if (canMove(Model.turn(directionToFox, 1)) && distance(i) != 1)
-                        return Model.turn(directionToFox, 1);
-                    else if (canMove(Model.turn(directionToFox, 0)) && distance(i) != 0)
-                        return Model.turn(directionToFox, 0);
-                    else if (canMove(Model.turn(directionToFox, 4)) && distance(i) != 4)
-                        return Model.turn(directionToFox, 4);
-                }
-//                } else if (look(i)==Model.EDGE && distance(i)<1) {
-//                    return Model.turn(i,4);
-////                    System.out.println("EDGE "+i);
-//                } else if (look(i)==Model.BUSH &&distance(i)<1) {
-//                    return Model.turn(i,1);
-//                }
+
+                if (canMove(Model.turn(directionToFox, 5)))
+                    return Model.turn(directionToFox, 5);
+                else if (canMove(Model.turn(directionToFox, 3)))
+                    return Model.turn(directionToFox, 3);
+                else if (canMove(Model.turn(directionToFox, 2)))
+                    return Model.turn(directionToFox, 2);
+                else if (canMove(Model.turn(directionToFox, 6)))
+                    return Model.turn(directionToFox, 6);
+                else if (canMove(Model.turn(directionToFox, 7)))
+                    return Model.turn(directionToFox, 7);
+                else if (canMove(Model.turn(directionToFox, 1)))
+                    return Model.turn(directionToFox, 1);
+                else if (canMove(Model.turn(directionToFox, 4)))
+                    return Model.turn(directionToFox, 4);
+                else if (canMove(Model.turn(directionToFox, 0)))
+                    return Model.turn(directionToFox, 0);
+            }
         }
         return Model.STAY;
     }
