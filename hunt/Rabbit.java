5f51e43048fdcb64ba359bb6bec4ce4a3c47def4
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-01-18 21:15:30 -0500

    <85

--- hunt/Rabbit.java
+++ hunt/Rabbit.java
@@ -16,28 +16,29 @@ public class Rabbit extends Animal {
                 if (look(i) == Model.FOX) {
                     directionToFox = i;
 
-                    if (canMove(Model.turn(directionToFox, 3)))
+                    if (canMove(Model.turn(directionToFox, 3)) && distance(i) != 3)
                         return Model.turn(directionToFox, 3);
-                else if (canMove(Model.turn(directionToFox,5)))
-                    return Model.turn(directionToFox,5);
-                else if (canMove(Model.turn(directionToFox,1)))
-                    return Model.turn(directionToFox,1);
-                else if (canMove(Model.turn(directionToFox,7)))
-                    return Model.turn(directionToFox,7);
-                else if (canMove(Model.turn(directionToFox,0)))
-                    return Model.turn(directionToFox,0);
-                else if (canMove(Model.turn(directionToFox,4)))
-                    return Model.turn(directionToFox,4);
-                else if (canMove(Model.turn(directionToFox,2)))
-                    return Model.turn(directionToFox,2);
-                else if (canMove(Model.turn(directionToFox,6)))
-                    return Model.turn(directionToFox,6);
-                } else if (look(i)==Model.EDGE && distance(i)<1) {
-                    return Model.turn(i,4);
-//                    System.out.println("EDGE "+i);
-                } else if (look(i)==Model.BUSH &&distance(i)<1) {
-                    return Model.turn(i,1);
+                    else if (canMove(Model.turn(directionToFox, 5)) && distance(i) != 5)
+                        return Model.turn(directionToFox, 5);
+                    else if (canMove(Model.turn(directionToFox, 2)) && distance(i) != 2)
+                        return Model.turn(directionToFox, 2);
+                    else if (canMove(Model.turn(directionToFox, 6)) && distance(i) != 6)
+                        return Model.turn(directionToFox, 6);
+                    else if (canMove(Model.turn(directionToFox, 7)) && distance(i) != 7)
+                        return Model.turn(directionToFox, 7);
+                    else if (canMove(Model.turn(directionToFox, 1)) && distance(i) != 1)
+                        return Model.turn(directionToFox, 1);
+                    else if (canMove(Model.turn(directionToFox, 0)) && distance(i) != 0)
+                        return Model.turn(directionToFox, 0);
+                    else if (canMove(Model.turn(directionToFox, 4)) && distance(i) != 4)
+                        return Model.turn(directionToFox, 4);
                 }
+//                } else if (look(i)==Model.EDGE && distance(i)<1) {
+//                    return Model.turn(i,4);
+////                    System.out.println("EDGE "+i);
+//                } else if (look(i)==Model.BUSH &&distance(i)<1) {
+//                    return Model.turn(i,1);
+//                }
         }
         return Model.STAY;
     }
