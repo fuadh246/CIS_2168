440220911bbc6e8397ebc21fdf9509ab22a555ef
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-02-06 23:28:47 -0500

    LinkedList

--- LinkedList_Exercises/src/LinkedList.java
+++ LinkedList_Exercises/src/LinkedList.java
@@ -1,5 +1,23 @@
 public class LinkedList <E> {
+    public static class Node<E>{
+        private E data;
+        private Node<E>next;
+        public Node(E data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
-
+        Node<String> n1 = new Node<>("Bread");
+        Node<String> n2 = new Node<>("Milk");
+        Node<String> n3 = new Node<>("Eggs");
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n1;
+        System.out.println(n1.data);
+        System.out.println(n1.next);
+        System.out.println(n2.data);
+        System.out.println(n2.next);
+        System.out.println(n3.data);
+        System.out.println(n3.next);
     }
 }
