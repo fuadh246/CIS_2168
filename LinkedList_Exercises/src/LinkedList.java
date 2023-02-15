896d38d85036b6c106f9ca63dc8e206be9cd8dd9
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-02-15 00:23:43 -0500

    02/15/2023

--- LinkedList_Exercises/src/LinkedList.java
+++ LinkedList_Exercises/src/LinkedList.java
@@ -1,4 +1,84 @@
+
+
 public class LinkedList <E> {
+    private int size;
+    private Node<E> head;
+//    private Node<E>tail;
+    public LinkedList(){
+        size =0;
+        head = null;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean add(E item){
+        this.add(size,item);
+        return true;
+    }
+    public void add(int index,E item){
+        if(index<0 || index>size){
+            throw new IndexOutOfBoundsException("Index bound");
+        }
+        Node <E> adding = new Node<>(item);
+        if(index==0){
+            adding.next=head;
+            head=adding;
+        }else {
+            Node <E> before = getNode(index-1);
+            adding.next=before.next;
+            before.next=adding;
+        }
+        size++;
+    }
+    public E remove(int index){
+        if(index<0 || index>size){
+            throw new IndexOutOfBoundsException("Index bound");
+        }
+        E toReturn =null;
+        if(index==0){
+            toReturn = head.data;
+            head =head.next;
+        }else {
+            Node <E> before = getNode(index-1);
+            toReturn = before.next.data;
+            before.next = before.next.next;
+        }
+        size--;
+        return toReturn;
+    }
+    private Node<E> getNode(int index){
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+    public String toString(){
+        String out="";
+        Node<E> current = head;
+        while (current!=null){
+            out+=current.data;
+            out+="-->";
+            current = current.next;
+        }
+        return out;
+    }
+    public E get(int index){
+        if(index<0|| index>=size){
+            throw new IndexOutOfBoundsException(":(");
+        }
+        return getNode(index).data;
+    }
+    public E set(int index,E item){
+        if(index<0|| index>=size){
+            throw new IndexOutOfBoundsException(":(");
+        }
+        Node <E> target = getNode(index);
+        E oldData = target.data;
+        target.data =item;
+        return oldData;
+    }
+
     public static class Node<E>{
         private E data;
         private Node<E>next;
@@ -6,18 +86,15 @@ public class LinkedList <E> {
             this.data = data;
         }
     }
+
     public static void main(String[] args) {
-        Node<String> n1 = new Node<>("Bread");
-        Node<String> n2 = new Node<>("Milk");
-        Node<String> n3 = new Node<>("Eggs");
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n1;
-        System.out.println(n1.data);
-        System.out.println(n1.next);
-        System.out.println(n2.data);
-        System.out.println(n2.next);
-        System.out.println(n3.data);
-        System.out.println(n3.next);
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(0,1);
+        list.add(1,2);
+        list.add(2,3);
+        list.add(3,4);
+        System.out.println(list.toString());
+        list.remove(1);
+        System.out.println(list.toString());
     }
 }
